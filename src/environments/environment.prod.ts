c09cfb04a57ce0189431cf6d41518c3daa6ed5a9
Your Name <you@example.com>
2026-02-19 23:15:27 +0000

    all

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
-export const environment = {
-  production: true,
-  apiUrl: '/.netlify/functions/groq'
-};
 
+export const environment = {
+  production: false,
+  apiKey: '/.netlify/functions/groq',
+  apiUrl: 'https://api.groq.com/openai/v1/chat/completions'
+};
